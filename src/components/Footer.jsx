2fa5580cb88eac49d131c9f8a8e80729cfa2636e
Kiril-Pavlov <Kire_P@yahoo.com>
2022-12-19 18:04:38 +0100

    added content to footer area

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
 
-const Footer = () => {
+import LogoImg from "../assets/logo.png"
+
+const Footer = ({footLinks}) => {
   return (
-    <div>Footer</div>
+    <footer>
+        <div>
+            <img src={LogoImg} alt="" />
+            <div>
+                <h3>USEFUL LINKS</h3>
+                <ul>
+                    <li><a href="">Terms of Services</a></li>
+                    <li><a href="">Privacy & Management of personal data</a></li>
+                    <li><a href="">Cookies policy</a></li>
+                    <li><a href="">FAQ</a></li>
+                </ul>
+            </div>
+            <div>
+                <h3>Home</h3>
+                <div>
+                    {footLinks.map(link=>(
+                        <a 
+                        href={link.linkTo} 
+                        key={link.linkName}
+                        className="font-primaryFont"
+                      >
+                        {link.linkName}
+                      </a>
+                    ))}
+                </div>
+            </div>
+        </div>
+    </footer>
   )
 }
 
