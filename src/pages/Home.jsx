7fde03441effe8545cf875207f94587c32ae0b87
Kiril-Pavlov <Kire_P@yahoo.com>
2022-12-23 23:15:44 +0100

    created home section and added  task points

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        Hero 1 section <br />
+        Hero 2 section <br />
+        Discover charities section <br />
+        Best charity section <br />
+        Why you should donate section
+    </div>
+  )
+}
+
+export default Home
