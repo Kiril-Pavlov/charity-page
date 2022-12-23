cbaa22e0e69426fe6ff10d94fabf3a8ba57fb410
Kiril-Pavlov <Kire_P@yahoo.com>
2022-12-23 23:22:09 +0100

    created hero section and added  task points

---
+++ src/components/HomeHero.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const HomeHero = () => {
+  return (
+    <div>
+        <div>
+            IMG <br />
+            Tab change text
+        </div>
+        <div>
+            Text <br />
+            IMG2
+        </div>
+    </div>
+  )
+}
+
+export default HomeHero
